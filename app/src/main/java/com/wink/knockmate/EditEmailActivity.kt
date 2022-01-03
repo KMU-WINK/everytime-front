d0c0504ec44b89abe43ee054eec9b81d655a6cfb
SangwonYoon <yoonsw0532@naver.com>
2022-01-04 00:29:20 +0900

    설정 화면에 있는 화면 이동 버튼 연결

---
+++ app/src/main/java/com/wink/knockmate/EditEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.wink.knockmate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editemail)
+    }
+}
--- app/src/main/java/com/wink/knockmate/SettingsActivity.kt
+++ app/src/main/java/com/wink/knockmate/SettingsActivity.kt
@@ -1,11 +1,47 @@
 package com.wink.knockmate
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val editProfileButton : Button = findViewById(R.id.editProfileButton)
+
+    private val editEmailButton : Button = findViewById(R.id.changeEmail)
+
+    private val editPasswordButton : Button = findViewById(R.id.changePassword)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        editProfileButton.setOnClickListener {
+            initEditProfileButton()
+        }
+
+        editEmailButton.setOnClickListener {
+            initEditEmailButton()
+        }
+
+        editPasswordButton.setOnClickListener {
+            initEditPasswordButton()
+        }
+    }
+
+    private fun initEditProfileButton(){
+        val intent = Intent(this,EditProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun initEditEmailButton(){
+        val intent = Intent(this,EditEmailActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun initEditPasswordButton(){
+        val intent = Intent(this,EditPasswordActivity::class.java)
+        startActivity(intent)
     }
 }
